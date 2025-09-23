5656b6f14caa9fc7c50b15d463e16003859085e2
ANTONIO RODRIGUES CARVALHO NETO <antonio.rcneto3@senacsp.edu.br>
2025-09-22 22:46:37 -0300

    aula 01

---
+++ aula01/helloworld/teste.ts
@@ -0,0 +1,9 @@
+const nome : string = "Joao Silva";
+
+const lista : Array<number> = [10, 20, 30];
+
+
+let tituloEleitor : string | undefined;
+
+console.log("Nome: ", nome);
+console.log("Lista: ", lista);
